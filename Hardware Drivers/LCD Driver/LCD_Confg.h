a0dc62a9817add1793fceb7a00a11c52a056256e
Mohamed Gebril <mohamedgebril024@gmail.com>
2018-09-20 19:54:57 +0200

    Create LCD_Confg.h

---
+++ Hardware Drivers/LCD Driver/LCD_Confg.h
@@ -0,0 +1,19 @@
+#ifndef LCD_CNFG_H
+#define LCD_CNFG_H
+
+
+// Put  groups ID
+#define LCD_RS 4
+#define LCD_EN 5
+#define LCD_D0 6
+#define LCD_D1 7
+#define LCD_D2 8
+#define LCD_D3 9
+#define LCD_D4 10
+#define LCD_D5 11
+#define LCD_D6 12
+#define LCD_D7 13
+
+//Attach R/W TO GND
+
+#endif
