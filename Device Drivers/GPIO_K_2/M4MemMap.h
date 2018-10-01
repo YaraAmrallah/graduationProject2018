c68006ba992b9bfd73d621a178808d39dae45e0f
Mohamed Gebril <mohamedgebril024@gmail.com>
2018-10-01 06:18:07 +0200

    Edited Wrong priority equation
    #define SET_INT_PRI(INT_NUM,PRI_NUM) PRI_REG(INT_NUM) &= ~(0x0F <<  (8*(INT_NUM%4)+5) ); 
    WAS.. #define SET_INT_PRI(INT_NUM,PRI_NUM) PRI_REG(INT_NUM) &= (0xF0 <<  (8*(INT_NUM%4)+5) );\

--- Device Drivers/GPIO_K_2/M4MemMap.h
+++ Device Drivers/GPIO_K_2/M4MemMap.h
@@ -41,7 +41,7 @@ typedef volatile uint32_t * const M4_PrefRegType;
 #define PRI_REG_BASE 0x400
 #define PRI_REG_NUM(INT_NUM) (INT_NUM/4) //63-29=34 (tarteeb el registers in data sheet)
 #define PRI_REG(INT_NUM) *((M4_PrefRegType)((PRI_REG_NUM(INT_NUM) * 4) + PRI_REG_BASE + M4_PREF_BASE_ADD))//addition of any number divided by 4
-#define SET_INT_PRI(INT_NUM,PRI_NUM) PRI_REG(INT_NUM) &= (0xF0 <<  (8*(INT_NUM%4)+5) );\
+#define SET_INT_PRI(INT_NUM,PRI_NUM) PRI_REG(INT_NUM) &= ~(0x0F <<  (8*(INT_NUM%4)+5) );\
                                      PRI_REG(INT_NUM) |= ((PRI_NUM & 0X0F)<< 8*(INT_NUM%4)+5)
 
 #define SWTRIG_REG   *((M4_PrefRegType)(M4_PREF_BASE_ADD + 0xF00))
