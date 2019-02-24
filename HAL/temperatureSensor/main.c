8e0c2c22a82a93b6f940d2850bf5d2d6a24df587
Yara Amr-Allah <yara.amrallah.94@gmail.com>
2019-02-24 22:26:10 +0200

    Update main.c

--- HAL/temperatureSensor/main.c
+++ HAL/temperatureSensor/main.c
@@ -9,17 +9,18 @@ int main(void)
 {
     GPIO_Init();
     ADC_Init();
-       tempSensorInitialization ();
-       float sensorReading;
-       uint16_t i = 0;
+    tempSensorInitialization ();
+    startADC_conversion(0);
+    float sensorReading;
+    uint16_t i = 0;
 
-       while(1)
-       {
-           tempSensorRequest(0);
-           sensorReading = returnTempSensorReading(0);
+    while(1)
+    {
+        mainTempSensorRequest (0);
+        sensorReading = returnTempSensorReading(0);
 
-           for(i = 0 ; i < 100 ; i++);
-       }
-       return 0;
+        for(i = 0 ; i < 100 ; i++);
+    }
+    return 0;
 
 }
