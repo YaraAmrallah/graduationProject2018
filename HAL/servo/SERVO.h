9f3931c38fc26505543dbc241df11a8441aa0f3c
AymanEskander <41763040+AymanEskander@users.noreply.github.com>
2019-02-27 23:57:50 +0200

    Update SERVO.h

--- HAL/servo/SERVO.h
+++ HAL/servo/SERVO.h
@@ -2,7 +2,7 @@
  * SERVO.h
  *
  *  Created on: Jan 29, 2019
- *      Author: Ayman-Pc
+ *      Author: Ayman
  */
 
 #ifndef SERVO_H_
@@ -13,6 +13,7 @@ PWM_CheckType Rotate_Servo (uint8_t PWM_ID, uint8_t degree );
 PWM_CheckType Initiate_Servo (uint8_t PWM_ID );
 PWM_CheckType AngleUp_Servo(uint8_t PWM_ID,uint8_t Pre_angle , uint8_t * Upcom_angle);
 PWM_CheckType AngleDown_Servo(uint8_t PWM_ID,uint8_t Pre_angle , uint8_t * Upcom_angle);
+
 
 
 #endif /* SERVO_H_ */
