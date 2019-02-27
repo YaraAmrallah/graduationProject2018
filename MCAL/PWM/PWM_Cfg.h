3e53cd7f9f4bcd6eeed3c2ea473d1fc4982d8521
AymanEskander <41763040+AymanEskander@users.noreply.github.com>
2019-02-28 00:07:30 +0200

    Update PWM_Cfg.h

--- MCAL/PWM/PWM_Cfg.h
+++ MCAL/PWM/PWM_Cfg.h
@@ -16,6 +16,7 @@
 /*A configuration to enable the clock division for the PWM module. */
 #define CLOCK_DIVISOR_ENABLE ENABLE /* ENABLE/DISABLE */
 #define CLOCK_DIVISOR_VALUE  DIVISOR_BY_64 /* DIVISOR_BY_N where N = {2,4,8,16,32,64} */
+#define ANGLE_SHIFT  1u
 
 
 #endif /* PWM_CFG_H_ */
